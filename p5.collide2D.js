f968d9298bb1e68e38435edacc868fbc9c0e2076
Ajax_007 <125272816+ajax2905@users.noreply.github.com>
2023-11-20 12:25:08 -0300

    Add files via upload

--- p5.collide2D.js
+++ p5.collide2D.js
@@ -1,6 +1,5 @@
 /*
-Repo:
-https://github.com/bmoren/p5.collide2D/
+Repo: https://github.com/bmoren/p5.collide2D/
 Created by http://benmoren.com
 Some functions and code modified version from http://www.jeffreythompson.org/collision-detection
 Version v0.7.3 | June 22, 2020
@@ -8,7 +7,7 @@ CC BY-NC-SA 4.0
 */
 
 
-console.log("ALLAN")
+console.log("Snow printer")
 
 p5.prototype._collideDebug = false;
 
